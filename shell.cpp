fa355a31f5d014f6bdaad9d66333549325672994
Jeremy <jeremywoffinden25@gmail.com>
2017-10-08 04:13:42 +0000

    started cd

--- shell.cpp
+++ shell.cpp
@@ -4,14 +4,17 @@
 #include <dirent.h>
 #include <algorithm>
 
-#define HOME_DIR "/home/jeremy/CS415/ShellLab"
-
 using namespace std;
 
+// GLOBAL VARIABLES
+const char HOME_DIR[128] = "home/jeremy";
+char CUR_DIR[128] = "/home/jeremy/CS415/ShellLab";
+
 // METHODS
-void parse(char* [], int); //parse command
-void echo(char* [], int); //echo
-void ls(); //ls
+void parse(char* [], int); //parse commands
+void echo(char* [], int); //echo implementation
+void ls(); //ls implementation
+void cd(char []); //cd implementation
 
 int main(){
   // VARIABLES
@@ -23,20 +26,20 @@ int main(){
   // INPUT LOOP
   while (true){
     tokenCount = 0;
-    cout << "> ";
+    cout << "[" << CUR_DIR << "]$ ";
     //get user input, store into "input"
     fgets(input, 128, stdin);
-    //check if input is "aexit" -- if so, break loop
-    if (strcmp(input,"aexit\n") == 0){
-      break;
-    }
     //split input into token array
     tok = strtok(input, " ");
     while (tok != NULL){
       split_input[tokenCount++] = tok;
       tok = strtok(NULL, " ");
     }
-    //split_input[tokenCount] = NULL;
+    split_input[tokenCount] = NULL;
+    //check if input is "aexit" -- if so, break loop
+    if (strcmp(split_input[0],"aexit\n") == 0 or strcmp(split_input[0],"aexit") == 0){ //with(out) whitespace
+      break;
+    }
     //parse
     parse(split_input, tokenCount);
   }
@@ -47,19 +50,20 @@ int main(){
 void parse(char* c[], int count){
   //if input is echo
   if (strcmp(c[0], "echo\n") == 0 or strcmp(c[0], "echo") == 0 ){
-    //printf("%d\n", strcmp(c[0], "echo"));
     echo(c, count);
   }
   else if (strcmp(c[0], "ls\n") == 0 or strcmp(c[0], "ls") == 0){
-    //printf("%d\n", strcmp(c[0], "ls\n"));
     ls();
+  }
+  else if (strcmp(c[0], "cd\n") == 0 or strcmp(c[0], "cd") == 0){
+    cd(c[1]);
   }
   else {
     cout << "Invalid Command" << endl;
   }
 }
 
-//echo function
+//echo function - just loop through rest of array
 void echo(char * inp[], int count){
   int counter = 1;
   while (counter < count){
@@ -72,18 +76,27 @@ void echo(char * inp[], int count){
   }
 }
 
-//ls function
+//ls function - using dirent.h
 void ls(){
   //create pointers for directory
   struct dirent *pointer;
   DIR *dir_path;
   //open directory
-  dir_path = opendir(HOME_DIR);
+  dir_path = opendir(CUR_DIR);
   //read each file from directory
   pointer = readdir(dir_path);
   while (pointer != NULL){
-    cout << pointer->d_name << endl;
+    cout << pointer->d_name << " ";
     pointer = readdir(dir_path);
   }
+  cout << endl;
   closedir(dir_path);
+}
+
+//cd function -- easier way to do it?
+void cd (char inp[]){
+  if (inp == NULL or inp == " "){
+    memcpy(CUR_DIR, HOME_DIR, 128);
+  }
+  cout << CUR_DIR << endl;
 }
